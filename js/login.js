dd8867cb290571b4ee17390c0492a9099aae95c0
Dan Ferg <89812588+Dawafe@users.noreply.github.com>
2022-01-06 11:50:48 -0700

    Next step: auth API

--- js/login.js
+++ js/login.js
@@ -17,6 +17,8 @@ class Login {
         }
       });
       if(error == 0) {
+// - !!! - this is where I need to add an authentication API
+// - !!! - the code below is just to allow the form to submit and move on
         localStorage.setItem("auth", 1);
         this.form.submit();
       }
